c8e0df67077db55276eb8aa9c5eca36a35c12019
LuckyLuuk035 <luckyluuk035@icloud.com>
2022-02-25 17:36:16 +0100

    P2 werkend

--- Main.py
+++ Main.py
@@ -1,23 +1,17 @@
 from Perceptron import Perceptron
-
-p1 = Perceptron("INVERT-gate perceptron")
-p1.bias = 0.5
-p1.weights = [-1]
-
-# print(p1.activate(-1))
-# print(p1)
-
-p2 = Perceptron("AND-gate")
-p2.bias = -1
-p2.weights = [0.5, 0.5]
-
-p3 = Perceptron("OR-gate")
-p3.bias = 0.5
-p3.w = [0, 0]  # were 0.5 0.5
+import random
 
 
-p3.update([[0, [0, 0]], [1, [0, 1]], [1, [1, 0]], [1, [1, 1]]])
+p1 = Perceptron("AND-gate")
+p1.bias = random.uniform(-1, 1)
+p1.w = [random.uniform(-1, 1), random.uniform(-1, 1)]
 
-p4 = Perceptron("figuur 2.8")
-p4.bias = -0.4
-p4.weights = [0.6, 0.3, 0.2]
+p2 = Perceptron("OR-gate")
+p2.bias = random.uniform(-1, 1)
+p2.w = [random.uniform(-1, 1), random.uniform(-1, 1)]
+
+print("AND-gate")
+p1.update([[0, [0, 0]], [0, [0, 1]], [0, [1, 0]], [1, [1, 1]]], 30)
+print("OR-gate")
+p2.update([[0, [0, 0]], [1, [0, 1]], [1, [1, 0]], [1, [1, 1]]], 30)
+
--- Perceptron.py
+++ Perceptron.py
@@ -13,30 +13,32 @@ class Perceptron:
     def __str__(self):
         return self.msg
 
-    def update(self, truthtable, epochs=5):
+    def update(self, truthtable, epochs=25, show=False):
         count = 0
         target = []
         input = []
         for i in truthtable:
             target.append(i[0])
             input.append(i[1])
-        
-        for i in range(len(target)):
-            while count <= epochs:
-                d = target[i]
-                x = input[i]
+
+        for i in range(epochs):
+            for row in truthtable:
+                d = target[count]  # set to row
+                x = input[count]
                 y = self.activate(x)  # output
-                print(self)
+
+                self.msg += str(d)
+                if show:
+                    print(self)
+                elif i+1 == epochs:
+                    print(self)
+
                 e = d - y
-
                 for j in range(len(self.w)):
-                    self.w[j] += 0.1 * e * y
-
-                db = 0.1 * e
-                self.b += db
+                    self.w[j] += 0.2 * e * x[j]
+                self.b += 0.2 * e
                 count += 1
             count = 0
-            print(x, "done.  Predicted output should be:", d)
 
     def activate(self, event):
         self.msg = ""
@@ -45,12 +47,12 @@ class Perceptron:
             self.msg += "'" + str(event[i]) + "i " + str(self.w[i]) + "w" + "'  "
             som = som + event[i]*self.w[i]
         self.msg += str(self.b) + "b  "
-        if float(som) + float(self.b) < 0:
-            self.msg += "--->  " + str(0) + "         "
+        if float(som) + float(self.b) <= 0:
+            self.msg += "--->  " + str(0) + "    t:   "
             return 0
-        elif float(som) + float(self.b) >= 0:
-            self.msg += "--->  " + str(1) + "         "
+        elif float(som) + float(self.b) > 0:
+            self.msg += "--->  " + str(1) + "    t:   "
             return 1
         else:  # in het geval van een error (Maak hier een 'try except' van)
-            self.msg += "  x   " + str(-1) + "        "
-            return -1
+            self.msg += "  x   " + str(-1) + "   t:   "
+            return False
