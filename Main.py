a8cf62d5fba066e23dc0baec39cc4adfdc399e40
LuckyLuuk035 <luckyluuk035@icloud.com>
2022-06-06 21:04:16 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -11,7 +11,9 @@ p2.bias = random.uniform(-1, 1)
 p2.w = [random.uniform(-1, 1), random.uniform(-1, 1)]
 
 print("AND-gate")
-p1.update([[0, [0, 0]], [0, [0, 1]], [0, [1, 0]], [1, [1, 1]]], 30)
-print("XOR-gate")
-p2.update([[0, [0, 0]], [1, [0, 1]], [1, [1, 0]], [0, [1, 1]]], 200, 0.5)
+p1.update([[0, [0, 0]], [0, [0, 1]], [0, [1, 0]], [1, [1, 1]]], 30, 0.2)
 
+print("XOR-gate")
+p2.update([[0, [0, 0]], [1, [0, 1]], [1, [1, 0]], [0, [1, 1]]], 200, 0.3)
+# Deze is niet mogelijk doordat een perceptron alleen maar lineare verdeelbare data kan leren.
+# als je een grafiek ziet zou je met 1 streep de 0 en 1 kunnen splitsen.
